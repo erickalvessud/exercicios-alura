082709a87e76977259f4aebf19f5a9e93e9667d3
Erick <ealves@magnasistemas.com.br>
2016-11-28 17:51:38 -0200

    Removing

--- introducao/intro.js
+++
@@ -1,3 +0,0 @@
-var nome = prompt("Digite seu nome");
-console.log("Seu nome é " + nome);
-console.log(typeof "bem-vindo");
--- projeto/imprime-nome.js
+++
@@ -1,8 +0,0 @@
-var trPacientes = document.getElementsByClassName("paciente");
-
-percorreArray(trPacientes, function (trAtual){
-	
-	var paciente = montaPaciente(trAtual);
-	//só imprimindo nome do paciente                        
-    console.log(paciente.nome);  
-});
